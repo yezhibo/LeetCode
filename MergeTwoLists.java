02a802494aee42c48425a2e1dfce978dd1ba5a14
yezhibo <1240475669@qq.com>
2021-07-29 23:01:10 +0800

    修改人：yezhibo 修改内容：合并两个有序链表

---
+++ MergeTwoLists.java
@@ -0,0 +1,49 @@
+package com.yezhibo.leetcode;
+
+/**
+ * 合并两个有序链表
+ * 题目描述：将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
+ * 思路：
+ * 按顺序遍历两个链表，每次取链表中数值较小的数，直到链表遍历完毕
+ *
+ */
+public class MergeTwoLists {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 哨兵节点
+        ListNode preHead = new ListNode(-1);
+        ListNode prev = preHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return preHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode l3 = mergeTwoLists(l1, l2);
+        System.out.println(l3.val);
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
