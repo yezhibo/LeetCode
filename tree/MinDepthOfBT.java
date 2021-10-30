8a25e1d7d5a27d79f2022a8505e1c44dde1af8aa
yezhibo <1240475669@qq.com>
2021-10-30 22:30:57 +0800

    修改人：yezhibo 修改内容：dfs

--- tree/MinDepthOfBT.java
+++ tree/MinDepthOfBT.java
@@ -5,11 +5,12 @@ import java.util.Stack;
 /**
  * 111.二叉树的最小深度
  * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
- *
+ * <p>
  * 题目描述：给定一个二叉树，找出其最小深度
  */
 public class MinDepthOfBT {
     int min = 0;
+
     public int minDepth(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
